4b101a31fb4df905e61a6cf7409190c76605fe58
Timemash <timemash24@gmail.com>
2022-10-06 01:24:55 +0900

    Use head to edit title Use Layout.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import NavBar from '../components/NavBar';
+import Head from 'next/head';
+import Seo from '../components/Seo';
 
 export default function Home() {
   return (
     <div>
+      <Seo title="home" />
       <h1 className="active">Home</h1>
     </div>
   );
