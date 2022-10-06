e31324b346f8382aea5d89fe2d5052d3aa740eb0
Timemash <timemash24@gmail.com>
2022-10-07 00:46:01 +0900

    Catch all example

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,7 @@ const API_KEY = process.env.API_KEY;
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -31,15 +23,7 @@ export default function Home({ results }) {
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           ></img>
           <h4>
-            <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                },
-              }}
-              as={`/movies/${movie.id}`}
-            >
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               <a>{movie.original_title}</a>
             </Link>
           </h4>
--- pages/movies/[id].js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function Detail() {
-  const router = useRouter();
-
-  return <h4>{router.query.title || 'Loading...'}</h4>;
-}
