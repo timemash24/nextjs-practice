053f4256fe9125a5356aab100cfec979e6e4a023
Timemash <timemash24@gmail.com>
2022-10-06 21:16:57 +0900

    Load movies from api using redirects, rewrites

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const API_KEY = 'b0ff54ec68ce385a475b7ddf13c81e4a';
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/contact/:path*',
+        destination: '/form/:path*',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,50 @@
-import { useState } from 'react';
-import NavBar from '../components/NavBar';
-import Head from 'next/head';
 import Seo from '../components/Seo';
+import { useEffect, useState } from 'react';
+
+const API_KEY = process.env.API_KEY;
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(`/api/movies`)).json();
+
+      setMovies(results);
+    })();
+  }, []);
   return (
-    <div>
+    <div className="container">
       <Seo title="home" />
-      <h1 className="active">Home</h1>
+      {!movies.length && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          ></img>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
