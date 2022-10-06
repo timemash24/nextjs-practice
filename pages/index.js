9fa4f885560256e34c5fb158bcbcfe9b1977924f
Timemash <timemash24@gmail.com>
2022-10-06 22:07:22 +0900

    SSR example using getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,14 @@ import { useEffect, useState } from 'react';
 
 const API_KEY = process.env.API_KEY;
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="home" />
-      {!movies.length && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           ></img>
           <h4>{movie.original_title}</h4>
         </div>
@@ -30,6 +21,9 @@ export default function Home() {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
@@ -47,4 +41,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
